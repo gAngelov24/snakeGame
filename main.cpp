8c8d45cdc5a4abf23e6ca52db3e6dee368d7e284
George Angelov <georgeangelov2@gmail.com>
2025-03-16 21:42:28 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x,y, fruitX, fruitY, score;
+
+ 
+void Setup()
+{
+    gameOver = false;
+}
+void Draw()
+{
+
+}
+void Input()
+{
+
+}
+void Logic()
+{
+
+}
+int main()
+{
+    Setup();
+    while(!gameOver){
+        Draw();
+        Input();
+        Logic();
+        //Sleep(10)
+    }
+
+    return 0;
+}
